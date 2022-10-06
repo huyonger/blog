8718893af0feb4c0f3fa71c2b77f20e66e4593de
YongHu <2385410383@qq.com>
2022-10-06 14:33:19 +0800

    fix(html-compress-middleware): remove node-html-parse

--- src/common/middleware/html-compress.js
+++ src/common/middleware/html-compress.js
@@ -1,46 +1,49 @@
-const { parse } = require('node-html-parser');
+// eslint-disable-next-line no-unused-vars
+var { parse } = require('node-html-parser');
 
 module.exports = (opts, app) => {
     return (ctx, next) => {
         return next().then(() => {
             let content = ctx.body;
-            let root = parse(content);
-            ctx.body = compressHTML(root);
+            // let root = parse(content);
+            ctx.body = compressHTML(content);
         });
     };
 };
 
-const token_def_val = {
-    link: { media: 'screen' },
-    input: { type: 'text' },
-    form: { method: 'get' },
-    script: { type: 'text/javascript' },
-    meta: { charset: 'UTF-8', 'http-equiv': 'X-UA-Compatible' },
-};
-function compressHTML(node) {
-    // 1.删除标签特定属性
-    node.removeWhitespace();
-    compressHTMLTokenDefVal(node);
+// const token_def_val = {
+//     link: { media: 'screen' },
+//     input: { type: 'text' },
+//     form: { method: 'get' },
+//     script: { type: 'text/javascript' },
+//     meta: { charset: 'UTF-8', 'http-equiv': 'X-UA-Compatible' },
+// };
+
+function compressHTML(content) {
+    // 1.删除标签特定属性 node-html-parser解析有问题舍弃
+    // node.removeWhitespace();
+    // compressHTMLTokenDefVal(node);
     // 2.删除特定闭合标签
-    let content = node.toString();
+    // let content = node.toString();
     content = content.replace(/<\/(html|body|colgroup|thead|tr|tbody|td|p|dt|dd|li|option|tfoot)>/g, '');
     // 3.删除特定only_name属性
     content = content.replace(/(disable|selected|checked|readonly|multiple)/g, '');
-    content = content.replace(/\n|\r/g, '');
+    // 4.删除空格、制表符、换行符
+    content = content.replace(/\n|\r|\t/g, '');
     return content;
 }
 
-function compressHTMLTokenDefVal(node) {
-    if (node.nodeType === 1 && node.tagName) {
-        const tagName = node.tagName.toLowerCase();
-        const removeAttributes = token_def_val[tagName] || [];
-        for (let key in removeAttributes) {
-            if (node.getAttribute(key) === removeAttributes[key]) {
-                node.removeAttribute(key);
-            }
-        }
-    }
-    for (let childNode of node.childNodes) {
-        compressHTMLTokenDefVal(childNode);
-    }
-}
+// function compressHTMLTokenDefVal(node) {
+//     if (node.nodeType === 1 && node.tagName) {
+//         const tagName = node.tagName.toLowerCase();
+//         const removeAttributes = token_def_val[tagName] || [];
+//         for (let key in removeAttributes) {
+//             if (node.getAttribute(key) === removeAttributes[key]) {
+//                 node.removeAttribute(key);
+//             }
+//         }
+//     }
+//     for (let childNode of node.childNodes) {
+//         compressHTMLTokenDefVal(childNode);
+//     }
+// }
